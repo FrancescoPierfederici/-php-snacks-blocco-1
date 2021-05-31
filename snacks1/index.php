a6f26f6b5406b2d5f20c443553791fabed894d38
FrancescoPierfederici <francescopierf@gmail.com>
2021-05-31 16:28:03 +0200

    snacks1

---
+++ snacks1/index.php
@@ -0,0 +1,19 @@
+<!-- ## Snack 1
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60 -->
+
+
+
+<?php
+$partitebasket=["Olimpia Milano - Cantù | 55-60","Virtus Bologna- Varese | 70-62","Pesaro - Reyer Venezia| 50-60","Stella Azzurra - Virtus Roma| 64-60","Fortitudo Bologna - Reggiana | 75-50" ];
+
+
+// echo  $partitebasket;
+var_dump($partitebasket)
+
+
+
+
+
+?>
+
